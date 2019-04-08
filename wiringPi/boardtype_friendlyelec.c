4ce146cdf71e9b455ec18f7dc08976a4142e982e
Nazar <troyan3@gmail.com>
2019-04-08 12:45:49 +0300

    Improve formatting of predefined structures

--- wiringPi/boardtype_friendlyelec.c
+++ wiringPi/boardtype_friendlyelec.c
@@ -9,80 +9,80 @@ const char* allwinner_tempfile = "/sys/class/thermal/thermal_zone0/temp";
 #define LOGE printf
 
 BoardHardwareInfo gAllBoardHardwareInfo[] = {
-    {"MINI6410", -1, S3C6410_COMMON, "S3C6410_Board",""},
-    {"MINI210", -1, S5PV210_COMMON, "S5PV210_Board",""},
-    {"TINY4412", -1, S5P4412_COMMON, "S5P4412_Board",""},
-    {"mini2451", 0, S3C2451_COMMON, "S3C2451_Board", ""},
+    {"MINI6410", -1, S3C6410_COMMON, "S3C6410_Board", ""},
+    {"MINI210",  -1, S5PV210_COMMON, "S5PV210_Board", ""},
+    {"TINY4412", -1, S5P4412_COMMON, "S5P4412_Board", ""},
+    {"mini2451",  0, S3C2451_COMMON, "S3C2451_Board", ""},
 
     //s5p4418
-    {"nanopi2", 0, NanoPi2, "NanoPi2",""},
-    {"nanopi2", 1, NanoPC_T2, "NanoPC-T2",""},
-    {"nanopi2", 2, NanoPi_S2, "NanoPi-S2",""},
-    {"nanopi2", 3, Smart4418, "Smart4418",""},
-    {"nanopi2", 4, NanoPi2_Fire, "NanoPi2-Fire",""},
-    {"nanopi2", 5, NanoPi_M2, "NanoPi-M2",""},
-    {"nanopi2", 7, NanoPi_M2A, "NanoPi-M2A",""},
-    {"nanopi2", 0x103, Smart4418SDK, "Smart4418SDK",""},
+    {"nanopi2",     0, NanoPi2,      "NanoPi2",      ""},
+    {"nanopi2",     1, NanoPC_T2,    "NanoPC-T2",    ""},
+    {"nanopi2",     2, NanoPi_S2,    "NanoPi-S2",    ""},
+    {"nanopi2",     3, Smart4418,    "Smart4418",    ""},
+    {"nanopi2",     4, NanoPi2_Fire, "NanoPi2-Fire", ""},
+    {"nanopi2",     5, NanoPi_M2,    "NanoPi-M2",    ""},
+    {"nanopi2",     7, NanoPi_M2A,   "NanoPi-M2A",   ""},
+    {"nanopi2", 0x103, Smart4418SDK, "Smart4418SDK", ""},
 
     //s5p6818
-    {"nanopi3", 1, NanoPC_T3, "NanoPC-T3",""},
-    {"nanopi3", 2, NanoPi_M3B, "NanoPi-M3B",""},
-    {"nanopi3", 3, Smart6818, "Smart6818",""},
-    {"nanopi3", 4, NanoPC_T3T, "NanoPC-T3T",""},
-    {"nanopi3", 5, NanoPi_Fire3, "NanoPi-Fire3",""},
-    {"nanopi3", 7, NanoPi_M3, "NanoPi-M3",""},
+    {"nanopi3", 1, NanoPC_T3,    "NanoPC-T3",    ""},
+    {"nanopi3", 2, NanoPi_M3B,   "NanoPi-M3B",   ""},
+    {"nanopi3", 3, Smart6818,    "Smart6818",    ""},
+    {"nanopi3", 4, NanoPC_T3T,   "NanoPC-T3T",   ""},
+    {"nanopi3", 5, NanoPi_Fire3, "NanoPi-Fire3", ""},
+    {"nanopi3", 7, NanoPi_M3,    "NanoPi-M3",    ""},
 
     //allwinner h3
     // kernel 3.x
-    {"sun8i", 0, NanoPi_M1, "NanoPi-M1", "0(0)"},
-    {"sun8i", 0, NanoPi_NEO, "NanoPi-NEO", "1(0)"},
-    {"sun8i", 0, NanoPi_NEO_Air, "NanoPi-NEO-Air", "2(0)"},
-    {"sun8i", 0, NanoPi_M1_Plus, "NanoPi-M1-Plus", "3(0)"},
-    {"sun8i", 0, NanoPi_Duo, "NanoPi-Duo", "4(0)"},
+    {"sun8i", 0, NanoPi_M1,       "NanoPi-M1",       "0(0)"},
+    {"sun8i", 0, NanoPi_NEO,      "NanoPi-NEO",      "1(0)"},
+    {"sun8i", 0, NanoPi_NEO_Air,  "NanoPi-NEO-Air",  "2(0)"},
+    {"sun8i", 0, NanoPi_M1_Plus,  "NanoPi-M1-Plus",  "3(0)"},
+    {"sun8i", 0, NanoPi_Duo,      "NanoPi-Duo",      "4(0)"},
     {"sun8i", 0, NanoPi_NEO_Core, "NanoPi-NEO-Core", "5(0)"},
-    {"sun8i", 0, NanoPi_K1, "NanoPi-K1", "6(0)"},
-    {"sun8i", 0, NanoPi_Hero, "NanoPi-Hero", "7(0)"},
-    {"sun8i", 0, NanoPi_Duo2, "NanoPi-Duo2", "8(0)"},
-    {"sun8i", 0, NanoPi_R1, "NanoPi-R1", "9(0)"},
+    {"sun8i", 0, NanoPi_K1,       "NanoPi-K1",       "6(0)"},
+    {"sun8i", 0, NanoPi_Hero,     "NanoPi-Hero",     "7(0)"},
+    {"sun8i", 0, NanoPi_Duo2,     "NanoPi-Duo2",     "8(0)"},
+    {"sun8i", 0, NanoPi_R1,       "NanoPi-R1",       "9(0)"},
 
     // kernel 4.x
-    {"Allwinnersun8iFamily", 0, NanoPi_M1, "NanoPi-M1", "0(0)"},
-    {"Allwinnersun8iFamily", 0, NanoPi_NEO, "NanoPi-NEO", "1(0)"},
-    {"Allwinnersun8iFamily", 0, NanoPi_NEO_Air, "NanoPi-NEO-Air", "2(0)"},
-    {"Allwinnersun8iFamily", 0, NanoPi_M1_Plus, "NanoPi-M1-Plus", "3(0)"},
-    {"Allwinnersun8iFamily", 0, NanoPi_Duo, "NanoPi-Duo", "4(0)"},
+    {"Allwinnersun8iFamily", 0, NanoPi_M1,       "NanoPi-M1",       "0(0)"},
+    {"Allwinnersun8iFamily", 0, NanoPi_NEO,      "NanoPi-NEO",      "1(0)"},
+    {"Allwinnersun8iFamily", 0, NanoPi_NEO_Air,  "NanoPi-NEO-Air",  "2(0)"},
+    {"Allwinnersun8iFamily", 0, NanoPi_M1_Plus,  "NanoPi-M1-Plus",  "3(0)"},
+    {"Allwinnersun8iFamily", 0, NanoPi_Duo,      "NanoPi-Duo",      "4(0)"},
     {"Allwinnersun8iFamily", 0, NanoPi_NEO_Core, "NanoPi-NEO-Core", "5(0)"},
-    {"Allwinnersun8iFamily", 0, NanoPi_K1, "NanoPi-K1", "6(0)"},
-    {"Allwinnersun8iFamily", 0, NanoPi_Hero, "NanoPi-Hero", "7(0)"},
-    {"Allwinnersun8iFamily", 0, NanoPi_Duo2, "NanoPi-Duo2", "8(0)"},
-    {"Allwinnersun8iFamily", 0, NanoPi_R1, "NanoPi-R1", "9(0)"},
+    {"Allwinnersun8iFamily", 0, NanoPi_K1,       "NanoPi-K1",       "6(0)"},
+    {"Allwinnersun8iFamily", 0, NanoPi_Hero,     "NanoPi-Hero",     "7(0)"},
+    {"Allwinnersun8iFamily", 0, NanoPi_Duo2,     "NanoPi-Duo2",     "8(0)"},
+    {"Allwinnersun8iFamily", 0, NanoPi_R1,       "NanoPi-R1",       "9(0)"},
 
     // a64
     {"sun50iw1p1", 0, NanoPi_A64, "NanoPi-A64", "0"},
 
     //allwinner h5
     // kernel 3.x
-    {"sun50iw2", 4, NanoPi_NEO2, "NanoPi-NEO2", "1(0)"},
-    {"sun50iw2", 4, NanoPi_M1_Plus2, "NanoPi-M1-Plus2", "3(0)"}, 
+    {"sun50iw2", 4, NanoPi_NEO2,      "NanoPi-NEO2",      "1(0)"},
+    {"sun50iw2", 4, NanoPi_M1_Plus2,  "NanoPi-M1-Plus2",  "3(0)"},
     {"sun50iw2", 4, NanoPi_NEO_Plus2, "NanoPi-NEO-Plus2", "2(0)"},
     {"sun50iw2", 4, NanoPi_NEO_Core2, "NanoPi-NEO-Core2", "0(0)"},
-    {"sun50iw2", 4, NanoPi_K1_Plus, "NanoPi-K1-Plus", "4(0)"},
+    {"sun50iw2", 4, NanoPi_K1_Plus,   "NanoPi-K1-Plus",   "4(0)"},
 
     // kernel 4.x
-    {"Allwinnersun50iw2Family", 4, NanoPi_NEO2, "NanoPi-NEO2", "1(0)"},
-    {"Allwinnersun50iw2Family", 4, NanoPi_M1_Plus2, "NanoPi-M1-Plus2", "3(0)"},
+    {"Allwinnersun50iw2Family", 4, NanoPi_NEO2,      "NanoPi-NEO2",      "1(0)"},
+    {"Allwinnersun50iw2Family", 4, NanoPi_M1_Plus2,  "NanoPi-M1-Plus2",  "3(0)"},
     {"Allwinnersun50iw2Family", 4, NanoPi_NEO_Plus2, "NanoPi-NEO-Plus2", "2(0)"},
     {"Allwinnersun50iw2Family", 4, NanoPi_NEO_Core2, "NanoPi-NEO-Core2", "0(0)"},
-    {"Allwinnersun50iw2Family", 4, NanoPi_K1_Plus, "NanoPi-K1-Plus", "4(0)"},
+    {"Allwinnersun50iw2Family", 4, NanoPi_K1_Plus,   "NanoPi-K1-Plus",   "4(0)"},
 
     //k2
     {"Amlogic", 0, NanoPi_K2, "NanoPi-K2", ""},
 
     //t4
-    {"nanopi4", 0, NanoPC_T4, "NanoPC-T4",""},
-    {"nanopi4", 1, NanoPi_M4, "NanoPi-M4",""},
-    {"nanopi4", 2, NanoPC_T4, "NanoPC-T4",""},
-    {"nanopi4", 4, NanoPi_NEO4, "NanoPi-NEO4",""},
+    {"nanopi4", 0, NanoPC_T4,   "NanoPC-T4",   ""},
+    {"nanopi4", 1, NanoPi_M4,   "NanoPi-M4",   ""},
+    {"nanopi4", 2, NanoPC_T4,   "NanoPC-T4",   ""},
+    {"nanopi4", 4, NanoPi_NEO4, "NanoPi-NEO4", ""},
 };
 
 static int getFieldValueInCpuInfo(char* hardware, int hardwareMaxLen, char* revision, int revisionMaxLen )
